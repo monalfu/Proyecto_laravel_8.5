d59d26decdb5901c5c78bd7dbcd0abdb64fed022
montse <monalfu.developer@gmail.com>
2023-07-27 19:21:08 +0200

    Inicio configuración rutas para NoticiasController, falta para el softdelete y borrado. Modificación inyectando trait softdelete en modelo. Creación métodos para CRUD de NoticiasController.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\NoticiasController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +18,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(NoticiasController::class)->group(function() {
+    // Página de listado noticias publicadas
+    Route::get('/noticias', 'index')
+        ->name('noticias.index');
+
+    // Creación noticias
+    Route::get('/noticias/create', 'create')
+        ->name('noticias.create');
+
+    // Detalles de una noticia
+    Route::get('/noticias/{noticia}', 'show')
+        ->name('noticias.show');
+
+    // Guardar noticia
+    Route::post('/noticias', 'store')
+        ->name('noticias.store');
+
+    // Editar noticia
+    Route::get('/noticias/{noticia}/edit', 'edit')
+        ->name('noticias.edit');
+
+    // Actualización de noticia
+    Route::match(['PUT', 'PATCH'], 'noticias/{noticia}', 'update')
+        ->name('noticias.update');
+
+    // 
+
 });
