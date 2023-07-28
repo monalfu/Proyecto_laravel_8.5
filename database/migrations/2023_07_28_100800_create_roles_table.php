408c377f44f58847af0af7c97eacf321b4ebe973
Montse Alguacil <monalfu.developer@gmail.com>
2023-07-28 10:12:59 +0200

    Migración para table roles y role_user

---
+++ database/migrations/2023_07_28_100800_create_roles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('role', 32);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('role_id')->references('id')->on('roles')->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->primary(['user_id', 'role_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->dropForeign('role_user_role_id_foreign');
+            $table->dropForeign('role_user_user_id_foreign');
+        });
+
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role_user');    }
+}
